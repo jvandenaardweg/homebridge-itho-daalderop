3902300bbb4f4ae7705b109174eabe0a751e138d
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2023-03-01 10:27:44 +0100

    chore: only run test coverage report in console

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
       provider: 'istanbul',
       all: true,
       exclude: ['**/vitest.setup.ts', '**/mocks/**', 'src/index.ts'],
+      reporter: ['text'],
     },
   },
   resolve: {
